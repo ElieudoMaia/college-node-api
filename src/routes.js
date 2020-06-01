8a1b1a37f86226e968a7efa59c07eadbccce425a
ElieudoMaia <elieudo.maia@gmail.com>
2020-06-01 15:40:38 -0300

    Added data validation

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,82 @@
 const express = require('express')
 const routes = express.Router()
+const { celebrate, Joi, Segments } = require('celebrate');
 
 const CourseController = require('./controllers/CourseController')
 const StudentController = require('./controllers/StudentController')
 
-routes.get('/courses', CourseController.index)
-routes.get('/courses/:id', CourseController.show)
-routes.post('/courses', CourseController.store)
-routes.put('/courses/:id', CourseController.update)
-routes.delete('/courses/:id', CourseController.delete)
+// courses' routes
+routes.get('/courses', celebrate({
+    [Segments.QUERY]: {
+        page: Joi.number().optional()
+    }
+}), CourseController.index)
 
-routes.get('/students', StudentController.index)
-routes.get('/students/:id', StudentController.show)
-routes.post('/students', StudentController.store)
-routes.put('/students/:id', StudentController.update)
-routes.delete('/students/:id', StudentController.delete)
+routes.get('/courses/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().length(36).required()
+    })
+}), CourseController.show)
+
+routes.post('/courses', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        course_name: Joi.string().required(),
+        duration: Joi.number().integer().max(60).required()
+    })
+}), CourseController.store)
+
+routes.put('/courses/:id', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        course_name: Joi.string().required(),
+        duration: Joi.number().integer().max(60).required()
+    }),
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().length(36).required()
+    })
+}), CourseController.update)
+
+routes.delete('/courses/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().length(36).required()
+    })
+}), CourseController.delete)
+
+// students' routes
+routes.get('/students', celebrate({
+    [Segments.QUERY]: {
+        page: Joi.number().optional()
+    }
+}), StudentController.index)
+
+routes.get('/students/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().length(36).required()
+    })
+}), StudentController.show)
+
+routes.post('/students', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        registration: Joi.string().required(),
+        course_id: Joi.string().length(36).required()
+    })
+}), StudentController.store)
+
+routes.put('/students/:id', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        registration: Joi.string().required(),
+        course_id: Joi.string().length(36).required()
+    }),
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().length(36).required()
+    })
+}), StudentController.update)
+
+routes.delete('/students/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.string().length(36).required()
+    })
+}), StudentController.delete)
 
 module.exports = routes
