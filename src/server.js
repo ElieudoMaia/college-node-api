0e98d2850baa3442e04b7d25d0e9d1a96952ba8e
ElieudoMaia <elieudo.maia@gmail.com>
2020-09-07 09:32:38 -0300

    add first test on courses

---
+++ src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app')
+
+const port = process.env.APP_PORT || 3333
+
+app.listen(port)
--- test/Course.spec.js
+++ test/Course.spec.js
@@ -1,5 +1,18 @@
-describe('Test', () => {
-    it('shoud run without errors', () => {
+const request = require('supertest')
+const { assert } = require('chai')
+const app = require('../src/app.js')
+
+describe('GET /courses', () => {
+    it('should list all courses with a object Array', async () => {
         
+        const { body } = await request(app)
+            .get('/courses')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+
+        assert.typeOf(body, 'Array')
+        assert.property(body[0], 'id', 'ID property is not present on object')
+
     })
 })
