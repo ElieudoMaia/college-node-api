546711548800211a11967c2e22d6c4956eb297cc
Elieudo Maia <elieudo.maia@g4flex.com.br>
2021-09-20 07:02:15 -0300

    chore: change to postgres database

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,16 @@
 const knex = require('knex')
 const config = require('config')
+const { logger } = require('../loaders/logger')
 
 const dbConfig = config.get('database')
 
-const connection = knex(dbConfig)
+const dbConnection = knex(dbConfig)
 
-module.exports = connection
+dbConnection.raw('SELECT 1').then(() => {
+  logger.info('Database connected');
+})
+.catch((e) => {
+  logger.error(`Database error: ${e.message}`);
+});
+
+module.exports = dbConnection
