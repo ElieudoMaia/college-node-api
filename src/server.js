b2887e06874021f81b31e56b77d71c86028ecd5d
Elieudo Maia <elieudo.maia@g4flex.com.br>
2021-09-20 06:27:17 -0300

    chore: add pino log lib

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const app = require('./app')
+const { logger } = require('./loaders/logger')
 
 const port = process.env.APP_PORT || 3333
 
-app.listen(port)
+app.listen(port, () => {
+  logger.info(`App running on port ${port}`)
+})
